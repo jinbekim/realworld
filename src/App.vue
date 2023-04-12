640e57a37c0f8c38475571ba3c88857a3a24cc1e
jinbekim <jinbekim@student.42seoul.kr>
2023-04-12 22:13:23 +0900

    add template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import Header from "./components/layouts/Header.vue";
+import Footer from "./components/layouts/Footer.vue";
 </script>
 
 <template>
   <Header></Header>
   <RouterView />
+  <Footer></Footer>
 </template>
