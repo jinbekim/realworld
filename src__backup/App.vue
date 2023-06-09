d6cc878d713b61fa6348fae6c97859ee8898794c
Likilee <gnsdlrl@daum.net>
2023-06-09 16:46:30 +0900

    기훈 1회

---
+++ src__backup/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import TheHeader from "./components/layouts/TheHeader.vue";
+import TheFooter from "./components/layouts/TheFooter.vue";
+import { onErrorCaptured } from "vue";
+
+// REVIEW - error boundary
+onErrorCaptured((err) => {
+  console.log(err);
+  console.log("error captured");
+  return false;
+});
+</script>
+
+<!-- REVIEW - The 템플릿에 하나  -->
+<template>
+  <TheHeader></TheHeader>
+  <RouterView />
+  <TheFooter></TheFooter>
+</template>
