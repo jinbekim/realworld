cc8798a81d0fa83c8b57b60a3d2392f1772d278f
jinbekim <jinbekim@student.42seoul.kr>
2023-09-03 19:50:37 +0900

    feat: ➕ tanstack query를 추가했습니다.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
+app.use(VueQueryPlugin);
 
 app.mount('#app');
